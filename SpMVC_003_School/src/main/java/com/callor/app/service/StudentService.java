34bab695d5184adec6bca433216155d3b591d931
hana825 <hana825@naver.com>
2022-04-22 16:51:05 +0900

    JSTL, Javascript Bubbling

---
+++ SpMVC_003_School/src/main/java/com/callor/app/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.callor.app.service;
+
+import java.util.List;
+
+import com.callor.app.domain.StudentVO;
+
+public interface StudentService {
+
+	public int insert(StudentVO studentVO); // 데이터 추가하기
+
+	public List<StudentVO> selectAll(); // 전체 리스트 가져오기
+
+	public StudentVO findById(String stNum); // 1개의 데이터 가져오기
+
+}
